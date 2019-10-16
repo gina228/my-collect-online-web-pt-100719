1178af79239998a6b822a47ffc4a27da4cc76353
Gina Graci <ginagraci92@gmail.com>
2019-10-16 01:52:03 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,11 @@
 def my_collect(collection)
-  my_collect(collection) do |name|
-  name.split(" ").first
+i = 0
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i += 1
+  end
+  new_collection
 end
-end 
+end
 
